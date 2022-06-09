d0e3bab25bcc6991f0e34e5564683a536908a47a
Park-minseo <jangmur41@gmail.com>
2022-06-10 00:32:17 +0900

    first commit

---
+++ app/src/main/java/com/pms/phrgb/MainActivity.java
@@ -0,0 +1,93 @@
+package com.pms.phrgb;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.palette.graphics.Palette;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    TextView textView;
+    int result = 0xFFFFFF;
+
+    private static final int REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    Uri uri = data.getData();
+                    Glide.with(getApplicationContext())
+                            .asBitmap()
+                            .load(uri)
+                            .into(new BitmapImageViewTarget(imageView) {
+                                @Override
+                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                    super.onResourceReady(resource, transition);
+
+
+                                    Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                                        @Override
+                                        public void onGenerated(@Nullable Palette palette) {
+                                            Palette.Swatch dominantSwatch = palette.getDominantSwatch();
+                                            result = dominantSwatch.getRgb();
+                                            textView.setText(Integer.toString(result, 10));
+
+                                        }
+                                    });
+                                }
+                            });
+
+
+                }
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "취소되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
